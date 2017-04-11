10a5741132c605761c40270e35700258123fa448
Nicolae Ziman <ziman_nicu@yahoo.com>
2017-04-11 17:43:38 +0200

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -499,9 +499,10 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // Moves the sliding background pizzas based on scroll position
 
 function updatePositions() {
-  var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  
   frame++;
   window.performance.mark("mark_start_frame");
+  var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
