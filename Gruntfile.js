cf238c39e86b4072bac3fe26a5ee45f1c5583431
Nicolae Ziman <ziman_nicu@yahoo.com>
2017-04-12 14:44:38 +0200

    Add files via upload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
 
-var ngrok = require(ngrok);
 module.exports = function(grunt) {
 
   // Project configuration.
@@ -7,15 +6,28 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
         build: {
-            src: 'js/perfmatters.js',
-            dest: 'js/perfmatters.min.js'
+            src: 'views/js/main.js',
+            dest: 'views/js/main.min.js'
         }
-    }
+    },
+      cssmin: {
+          target: {
+              files: [{
+                  expand: true,
+                  cwd: 'views/css/',
+                  src: ['*.css', '!*.min.css'],
+                  dest: 'views/css',
+                  ext: '.min.css'
+    }]
+  }
+}
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['cssmin']);
 };
